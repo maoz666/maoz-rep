d62882b58bca3de48ae685fce37bd9b3894e9996
maoz666 <59202235+maoz666@users.noreply.github.com>
2020-03-02 20:58:59 +0300

    upd

--- week2/w2t18.py
+++ week2/w2t18.py
@@ -10,8 +10,6 @@ a2 = int(input())
 b2 = int(input())
 c2 = int(input())
 
-if (a1, b1, c1, a2, b2, c2) <= (0, 0, 0, 0, 0, 0):
-    print('Boxes are incomparable')
 elif (a1 == a2 and b1 == b2 and c1 == c2) or (
         a1 == a2 and b1 == c2 and c1 == b2) or (
         a1 == b2 and b1 == a2 and c1 == c2) or (
@@ -26,5 +24,13 @@ elif (a1 <= a2 and b1 <= b2 and c1 <= c2) or (
         a1 <= c2 and b2 <= a2 and c1 <= b2) or (
         a1 <= c2 and b1 <= b2 and c1 <= a2):
     print('The first box is smaller than the second one')
-else:
+elif (a1 >= a2 and b1 >= b2 and c1 >= c2) or (
+        a1 >= a2 and b1 >= c2 and c1 >= b2) or (
+        a1 >= b2 and b1 >= a2 and c1 >= c2) or (
+        a1 >= b2 and b1 >= c2 and c1 >= a2) or (
+        a1 >= c2 and b2 >= a2 and c1 >= b2) or (
+        a1 >= c2 and b1 >= b2 and c1 >= a2):
     print('The first box is larger than the second one')
+else:
+    print('Boxes are incomparable')
+ 
